77ed23350cba192de9ed45526a737b18021cce53
JungJin Moon <105341553+Mjj4682@users.noreply.github.com>
2022-11-01 01:07:37 +0900

    [level 1] Title: 예산, Time: 0.06 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+    let answer = 0;
+    let sum = 0;
+    d.sort((a, b) => a - b)
+    for(let i = 0; i < d.length; i++){
+        sum = sum + d[i]
+        if(sum <= budget){
+            answer++
+        }
+    }
+    return answer;
+}
