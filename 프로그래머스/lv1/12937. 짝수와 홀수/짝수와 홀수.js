2406dbcf0b240c50acd712ca02021f7c7dd494c2
JungJin Moon <105341553+Mjj4682@users.noreply.github.com>
2022-11-01 00:47:26 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,9 +1,9 @@
 function solution(num) {
     let answer = '';
-    if(num % 2 == 1 || num % 2 == -1){
+    if(num % 2 === 1 || num % 2 === -1){
         answer = 'Odd'
     }
-    if(num % 2 == 0){
+    else if(num % 2 === 0){
         answer = 'Even'
     }
     return answer;
