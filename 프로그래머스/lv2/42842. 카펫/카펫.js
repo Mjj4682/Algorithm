c6af4242f6368b84564b1507dc21b74976f41fd3
JungJin Moon <105341553+Mjj4682@users.noreply.github.com>
2022-11-01 01:08:30 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -0,0 +1,11 @@
+function solution(brown, yellow) {
+    const sumAll = brown + yellow
+    const squtAll = Math.ceil(Math.sqrt(sumAll))
+    for(let i = squtAll; i < brown; i++){
+        for(let j = squtAll; j >= 1; j--){
+            if(i * j === sumAll && (i-2) * (j-2) == yellow){
+                return [i, j]
+            }
+        }
+    }
+}
