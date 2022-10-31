0038573135d31b9a4ed93cfa00251b1ae67724b2
JungJin Moon <105341553+Mjj4682@users.noreply.github.com>
2022-11-01 01:11:37 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 4.43 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,42 @@
+function solution(board, moves) {
+    var answer = 0;
+    let arrC = [];
+    let arrFinal = [];
+    let temp = '';
+    let count = true;
+    let i=0;
+    let j=0;
+    for(i=0; i<moves.length; i++){
+        temp = moves[i]-1
+        for(j=0; j<board.length; j++){
+            if(board[j][temp] != 0 ){
+                arrC.push(board[j][temp])
+                board[j][temp] = 0;
+                break;
+            }
+        }
+    }
+    console.log(arrC)
+    const arr = [...arrC];
+    console.log(arr)
+    while(count){
+        i=0;
+        j=1;
+        arrFinal = [];
+        while(i<arrC.length){
+            if(arrC[i] === arrC[j]){
+                i += 2;
+                j += 2;
+            }
+            else{
+                arrFinal.push(arrC[i])
+                i += 1;
+                j += 1;
+            }
+        }
+        if(arrC.length === arrFinal.length) count =false;
+        else arrC = arrFinal;
+    }
+    // console.log(arr, arrC)
+    return arr.length - arrFinal.length;
+}
