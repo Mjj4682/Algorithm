22696cb810b238bb079449ce1723cdc63a6dad79
JungJin Moon <105341553+Mjj4682@users.noreply.github.com>
2022-11-06 22:55:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.56 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,7 +1,7 @@
 function solution(n, words) {
     const checkArr =[]
     for(let i = 1; i < words.length; i++){
-        checkArr.push(words[i-1])
+        checkArr.push(words[i-1]);
         if(words[i].charAt(0) !== words[i-1].charAt(words[i-1].length - 1)){
             return([i % n + 1, (parseInt(i / n)) + 1])
         }
